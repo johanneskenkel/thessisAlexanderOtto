be37b28099c7816477f77156c56a1c69183172cd
Alexander Otto <alexander.otto@syngenio.de>
2023-01-27 11:54:31 +0100

    first commit

---
+++ java_demo/src/main/java/org/hbrs/thesis/model/Person.java
@@ -0,0 +1,116 @@
+package org.hbrs.thesis.model;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String birthDay;
+    private String addDate;
+
+    public Person(int id, String firstName, String lastName, int age, String birthDay, String addDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.birthDay = birthDay;
+        this.addDate = addDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(String addDate) {
+        this.addDate = addDate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (id != other.id)
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (age != other.age)
+            return false;
+        if (birthDay == null) {
+            if (other.birthDay != null)
+                return false;
+        } else if (!birthDay.equals(other.birthDay))
+            return false;
+        if (addDate == null) {
+            if (other.addDate != null)
+                return false;
+        } else if (!addDate.equals(other.addDate))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + age;
+        result = prime * result + ((birthDay == null) ? 0 : birthDay.hashCode());
+        result = prime * result + ((addDate == null) ? 0 : addDate.hashCode());
+        return result;
+    }
+}
---
+++ java_demo/src/test/java/org/hbrs/thesis/service/PersonServiceTest.java
@@ -0,0 +1,8 @@
+package org.hbrs.thesis.service;
+
+import org.junit.jupiter.api.Test;
+
+class PersonServiceTest {
+    // @Test
+    // assureThatPersonServiceGenerated
+}
