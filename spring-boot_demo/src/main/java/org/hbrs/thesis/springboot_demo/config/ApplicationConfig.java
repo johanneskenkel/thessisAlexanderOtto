2b1b6e1c3333c7b11f393444f24264cdd8aca646
Alexander Otto <sashaotto2000@gmail.com>
2023-03-09 11:41:36 +0100

    fix application config

--- spring-boot_demo/src/main/java/org/hbrs/thesis/springboot_demo/config/ApplicationConfig.java
+++ spring-boot_demo/src/main/java/org/hbrs/thesis/springboot_demo/config/ApplicationConfig.java
@@ -1,7 +1,9 @@
 package org.hbrs.thesis.springboot_demo.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ApplicationConfig {
     @Value("postgres.table")
     private String postgresTable;
